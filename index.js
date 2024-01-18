20f3f06774bb6452c5c60145acc230bb9961c8a2
Seny Irinarkhova <kseniiaIrinarkhova@gmail.com>
2024-01-18 15:10:26 -0800

    added tast for part 2

--- index.js
+++ index.js
@@ -67,3 +67,18 @@ for (const symbol of str) {
             break;
     }
 }
+
+
+/** Part 2: Expanding Functionality
+
+Declare a variable that stores the number of columns in each row of data within the CSV.
+Instead of hard-coding four columns per row, expand your code to accept any number of columns. This should be calculated dynamically based on the first row of data.
+
+Store your results in a two-dimensional array.
+    Each row should be its own array, with individual entries for each column.
+    Each row should be stored in a parent array, with the heading row located at index 0.
+Cache this two-dimensional array in a variable for later use.
+
+input: ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26
+*/
+
